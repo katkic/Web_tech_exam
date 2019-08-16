37d8066d67710b06942edebe5717590da0cd8f31
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-08-16 11:17:24 +0900

    フォーマットの調整

--- goya.rb
+++ goya.rb
@@ -4,7 +4,6 @@ cgi = CGI.new
 
 # データを受け取った後、HTMLの形式でレスポンス（反応）を返す
 cgi.out('type' => 'text/html', 'charset' => 'UTF-8') do
-  # 受け取ったデータをcgi['input']で取り出し、ローカル変数に代入する
   title = cgi['title']
   get = cgi['goya']
 
