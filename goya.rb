22eb54b7a76e9d7c63b0b2c5cf92741efe3e97ce
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-08-16 11:07:52 +0900

    ボタンごとのゴーヤの表示を実装

--- goya.rb
+++ goya.rb
@@ -5,13 +5,14 @@ cgi = CGI.new
 # データを受け取った後、HTMLの形式でレスポンス（反応）を返す
 cgi.out('type' => 'text/html', 'charset' => 'UTF-8') do
   # 受け取ったデータをcgi['input']で取り出し、ローカル変数に代入する
+  title = cgi['title']
   get = cgi['goya']
 
   # HTMLでレスポンスを返却する
   "<HTML>
     <body>
-      <p>受け取った文字列は下記になります</p>
-      <p>文字列：#{get}</p>
+      <h1>#{title}</h1>
+      <p>#{get}</p>
     </body>
   <HTML>"
 end
